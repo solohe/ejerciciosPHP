12c99394f7b272be5c0c839ffbba5aefff5b2e49
Carlos <carlos.e.santaniello@gmail.com>
2019-11-09 20:31:07 +0100

    Agregada primera parte de ArrayBidimensionalAsociativo.php

---
+++ Recorrido de Arrays/ArrayBidimensionalAsociativo.php
@@ -0,0 +1,45 @@
+<?php
+/* $productos =
+array(array("Codigo"=> "CB", "Descripcion" =>" Cebollas","Precio"=> 100),
+ //0codigo-0descripcion-0precio
+ array("Codigo"=> "AC","Descripcion" => "Aceite","Precio"=> 120),
+ //1codigo-1descripcion-1precio
+ array("Codigo"=> "MZ","Descripcion" => "Manzanas", "Precio"=> 92)
+ //2codigo-2descripcion-2precio
+);
+print_r($productos);
+echo "<br>";echo "<br>";echo "<br>";
+$productos2 =
+array("prod1" =>array("Codigo"=> "CB", "Descripcion" =>" Cebollas","Precio"=> 100),
+ //prod1codigo-prod1descripcion-prod1precio
+ "prod2" =>array("Codigo"=> "AC","Descripcion" => "Aceite","Precio"=> 120),
+ //prod2codigo-prod2descripcion-prod2precio
+ "prod3" =>array("Codigo"=> "MZ","Descripcion" => "Manzanas", "Precio"=> 92)
+ //prod3codigo-prod3descripcion-prod3precio
+);
+print_r($productos2);
+echo "<br>";echo "<br>";echo "<br>";
+Realizar para las dos variables $productos y $productos2:
+m) Recorrido mediante el uso de variables.
+n) Recorrido mediante el uso de un bucle while y otro for.
+o) Recorrido mediante el uso de dos foreach.
+*/
+
+array(array("Codigo"=> "CB", "Descripcion" =>" Cebollas","Precio"=> 100),
+ //0codigo-0descripcion-0precio
+ array("Codigo"=> "AC","Descripcion" => "Aceite","Precio"=> 120),
+ //1codigo-1descripcion-1precio
+ array("Codigo"=> "MZ","Descripcion" => "Manzanas", "Precio"=> 92)
+ //2codigo-2descripcion-2precio
+);
+
+$productos2 =
+array("prod1" =>array("Codigo"=> "CB", "Descripcion" =>" Cebollas","Precio"=> 100),
+ //prod1codigo-prod1descripcion-prod1precio
+ "prod2" =>array("Codigo"=> "AC","Descripcion" => "Aceite","Precio"=> 120),
+ //prod2codigo-prod2descripcion-prod2precio
+ "prod3" =>array("Codigo"=> "MZ","Descripcion" => "Manzanas", "Precio"=> 92)
+ //prod3codigo-prod3descripcion-prod3precio
+);
+
+?>
