3074ea4b7408d22e19e128c586768fd35129c8d5
Carlos Santaniello <carlos.e.santaniello@gmail.com>
2019-11-19 21:56:24 +0100

    Update ejercicios4.php
    Added comments to array functions

--- Arrays/Ejercicio4.php
+++ Arrays/Ejercicio4.php
@@ -1,19 +1,11 @@
 <?php
 //Realizar las pruebas de las funciones anteriores. Mostrar los resultados.
-
 $array = array('apellido', 'email', 'telefono');
-print_r(array_keys($array));
-
-print_r(array_values($array));
-
-print_r(in_array(email, $array));
-
-print_r(range(0, 2));
-
-print_r(array_reverse($array));
-
-print_r(shuffle($array));
-
-print_r(array_merge($array));
-
+print_r(array_keys($array)); //Devuelve un vector con todas las claves
+print_r(array_values($array)); //Devuelve un vector con todos los valores
+print_r(in_array(email, $array)); //Verifica si existe un elemento en el array
+print_r(range(0, 2)); //Crea un vector con un rango de min a max
+print_r(array_reverse($array)); //Invierte un array
+print_r(shuffle($array)); //Desordena un array
+print_r(array_merge($array)); //Mezcla arrays
 ?>
